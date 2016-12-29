378b99818f8cd9528707747ce38203868b0eafd4
Piotr Oniszczuk <warpme@o2.pl>
2016-12-29 11:04:22 +0100

    Update to v1.5

--- phone-notifier.py
+++ phone-notifier.py
@@ -7,16 +7,28 @@ import os
 import os.path
 import time
 import re
+import datetime
 
 debug = 0
 log_level = 3
 alarms_cfg = "/etc/phone-notifier/alarms.config"
 config_cfg = "/etc/phone-notifier/main.config"
+in_playback = 0
+in_call = 0
+in_request = 0
+player_id = 0
+in_deaf_period = 0
+deaf_duration = 0
 
 
+def log(str):
+    now = time.time()
+    localtime = time.localtime(now)
+    milliseconds = '%03d' % int((now - int(now)) * 1000)
+    timestamp = time.strftime('%H:%M:%S', localtime) + "." + milliseconds
+    print timestamp + " " + str
 
-
-print "\nPhone notifier v1.0\n(c) Piotr Oniszczuk\n"
+log("Phone notifier v1.5 (c) Piotr Oniszczuk\n")
 
 def LoadConfig( cfg_file ):
     file = open(cfg_file,"r")
@@ -46,6 +58,8 @@ bound_address    = config["bound_address"]
 rtp_port         = int(config["rtp_port"])
 semaphores_path  = config["semaphores_path"]
 sound_files_path = config["sound_files_path"]
+playback_duration = int(config["playback_duration"])
+deaf_time = int(config["deaf_time_duration"])
 
 print "VoIP config:"
 print "  -phone          : " + phone
@@ -56,6 +70,8 @@ if sip_proxy:
     print "  -SIP proxy      : " + sip_proxy
 if bound_address:
     print "  -Bound addr.    : " + bound_address
+print "Playback duration : " + str(playback_duration) + "sec"
+print "Deaf Time         : " + str(deaf_time) + "sec"
 print "Semaphores:"
 print "  -Semaphore pref : " + semaphores_path
 print "  -Sound file pref: " + sound_files_path
@@ -63,11 +79,12 @@ for sem in list(alarms):
     print "      " + sem + " -> " + alarms[sem]
 print " "
 
-# Logging callback
+call_duration = playback_duration
+
 def log_cb(level, str, len):
     print str,
 
-# Callback to receive events from AccoutRegistration
+
 class MyAccountCallback(pj.AccountCallback):
     sem = None
 
@@ -79,37 +96,68 @@ class MyAccountCallback(pj.AccountCallback):
         self.sem.acquire()
 
     def on_reg_state(self):
+        global in_request
         if self.sem:
             if self.account.info().reg_status >= 200:
                 self.sem.release()
+                log("Housekeeping: clearing in_request flag...")
+                in_request = 0
 
-# Callback to receive events from Call
+
 class MyCallCallback(pj.CallCallback):
+    in_playback = 0
+    in_call = 0
     def __init__(self, call=None):
         pj.CallCallback.__init__(self, call)
 
-    # Notification when call state has changed
     def on_state(self):
-        print "Call is ", self.call.info().state_text,
-        print "last code =", self.call.info().last_code,
-        print "(" + self.call.info().last_reason + ")"
+        global lib, call_duration, in_call, in_playback, in_request, player_id
+        log("Call_state=" + self.call.info().state_text + ", last_code=" + str(self.call.info().last_code) + "(" + self.call.info().last_reason + ")")
+        in_call = 0
+        if self.call.info().state == 6:
+            log("Call disconnected. Stoping " + str(playback_duration) + "sec. timer...")
+            call_duration = playback_duration
+            in_playback = 0
+            in_request = 0
+        if player_id:
+            player_id = 0
+            lib.player_destroy(player_id)
 
-    # Notification when call's media state has changed.
     def on_media_state(self):
-        global lib
+        global lib, call_duration, in_call, in_playback, in_request, player_id
         if self.call.info().media_state == pj.MediaState.ACTIVE:
-            # Get right sound file
-            snd_file = alarms[alarm]
-            snd_file = sound_files_path + "/" + snd_file
-            print "Will play: " + snd_file
-            # Create player
-            player_id = lib.create_player(snd_file, loop="True")
-            player_slot = lib.player_get_slot(player_id)
-            # Connect player to call
-            call_slot = self.call.info().conf_slot
-            lib.conf_connect(call_slot, 0)
-            lib.conf_connect(player_slot, call_slot)
-            print "Call estabilished. Playing: " + snd_file
+            log("Media openeded...")
+            call_duration = 0
+            in_call = 1
+            if not in_playback:
+                in_playback = 1
+                snd_file = alarms[alarm]
+                snd_file = sound_files_path + "/" + snd_file
+                log("Will play: " + snd_file)
+                try:
+                    log("Creating player...")
+                    player_id = lib.create_player(snd_file, loop="True")
+                except pj.Error, e:
+                    log("Exception: " + str(e))
+                    lib.destroy()
+                    lib = None
+                    sys.exit(1)
+                player_slot = lib.player_get_slot(player_id)
+                call_slot = self.call.info().conf_slot
+                lib.conf_connect(call_slot, 0)
+                lib.conf_connect(player_slot, call_slot)
+                log("Call estabilished. Playing: " + snd_file)
+            else:
+                log("Already playing...")
+        else:
+            in_playback = 0
+            in_call = 0
+            in_request = 0
+            call_duration = playback_duration
+            log("Media closed. Stoping " + str(playback_duration) + "sec. timer...")
+            if player_id:
+                player_id = 0
+                lib.player_destroy(player_id)
 
 lib = pj.Lib()
 
@@ -131,10 +179,7 @@ try:
     acc.set_callback(acc_cb)
     acc_cb.wait()
 
-    print "\n"
-    print "Registration complete, status=", acc.info().reg_status, \
-          "(" + acc.info().reg_reason + ")"
-
+    log("Registration complete, status=" + str(acc.info().reg_status) + "(" + str(acc.info().reg_reason) + ")")
 
     while True:
         if os.path.isfile(semaphores_path + "/exit.sem") and os.access(semaphores_path + "/exit.sem", os.R_OK):
@@ -145,18 +190,47 @@ try:
         else:
             for sem in list(alarms):
                 if os.path.isfile(semaphores_path + "/" + sem) and os.access(semaphores_path + "/" + sem, os.R_OK):
-                    print sem + " Semaphore file detected. Triggering call..."
                     alarm=sem
-                    os.remove(semaphores_path + "/" + sem)
-                    call = acc.make_call(phone, MyCallCallback())
+                    if in_deaf_period:
+                        os.remove(semaphores_path + "/" + sem)
+                        log("'" + sem + "' detected but now is in deaf period...")
+                    elif in_request:
+                        os.remove(semaphores_path + "/" + sem)
+                        log("'" + sem + "' detected but already servicing another call request...")
+                    else:
+                        log("'" + sem + "' semaphore file detected. Triggering call..")
+                        in_request = 1
+                        call = acc.make_call(phone, MyCallCallback())
+                        log("Starting to ignore triggers for " + str(deaf_time) + "sec deaf time...")
+                        in_deaf_period = 1
+                        deaf_duration = 0
+                        os.remove(semaphores_path + "/" + sem)
                 else:
                     if debug:
-                        print sem + " No semaphore detected..."
+                        log("No semaphore detected...")
 
         time.sleep(1)
 
+        if call_duration != playback_duration:
+            call_duration = call_duration + 1
+            #print str(call_duration) + "sec.of playback"
+            if call_duration == playback_duration:
+                if in_call == 1:
+                    log(str(playback_duration) + "sec. call duration reached. Ending call and stoping timer...")
+                    if player_id:
+                        player_id = 0
+                        lib.player_destroy(player_id)
+                    call.hangup()
+
+        if deaf_duration != deaf_time:
+            deaf_duration = deaf_duration + 1
+            if deaf_duration == deaf_time:
+                in_deaf_period = 0
+                log(str(deaf_time) + "sec.deaf time ended. Listening again for triggers...")
+
+
 except pj.Error, e:
-    print "Exception: " + str(e)
+    log("Exception: " + str(e))
     lib.destroy()
     lib = None
     sys.exit(1)
